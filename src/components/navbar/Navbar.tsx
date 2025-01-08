3c493e42ac1169c2503af6ba51a14881a13715e3
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-01-08 23:02:15 +0100

    fix: css

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import style from "./navbar.module.css";
 import avatar from "../../assets/image/photo_profil.jpg";
-import {Link} from "react-scroll";
+import { Link } from "react-scroll";
 
 export default function Navbar() {
 	return (
@@ -11,11 +11,27 @@ export default function Navbar() {
 				className={style.profilImg}
 			/>
 			<ul className={style.navbarList}>
-				<li><Link to="about" smooth={true} duration={600} offset={-50}>A PROPOS</Link> </li>
-				<li><Link to="skills" smooth={true} duration={600} offset={-50}>SKILLS</Link> </li>
-				<li><Link to="projects" smooth={true} duration={600} offset={-50}>PROJETS</Link> </li>
-				<li> <Link to="contact" smooth={true} duration={600} offset={-50}>CONTACT</Link> </li>
-				
+				<li>
+					<Link to="about" smooth={true} duration={600} offset={-50}>
+						A PROPOS
+					</Link>{" "}
+				</li>
+				<li>
+					<Link to="skills" smooth={true} duration={600} offset={-50}>
+						SKILLS
+					</Link>{" "}
+				</li>
+				<li>
+					<Link to="projects" smooth={true} duration={600} offset={-50}>
+						PROJETS
+					</Link>{" "}
+				</li>
+				<li>
+					{" "}
+					<Link to="contact" smooth={true} duration={600} offset={-50}>
+						CONTACT
+					</Link>{" "}
+				</li>
 			</ul>
 		</nav>
 	);
