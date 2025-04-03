1d1c295a60bc679892fda9fdbaca3baeef84876a
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-04-03 13:58:27 +0200

    feat: new project

--- src/components/cardVideo/CardVideo.tsx
+++ src/components/cardVideo/CardVideo.tsx
@@ -14,15 +14,17 @@ export default function CardVideo({ card }: { card: CardType }) {
   return (
     <div className={style.cardVideo}>
       <h3>{card.title}</h3>
-      <video width="90%" controls>
-        <source src={card.video} type="video/mp4" />
-        <track
-          kind="captions"
-          src="captions.vtt"
-          srcLang="fr"
-          label="Français"
-        />
-      </video>
+      {card.video !== "" && (
+        <video width="90%" controls>
+          <source src={card.video} type="video/mp4" />
+          <track
+            kind="captions"
+            src="captions.vtt"
+            srcLang="fr"
+            label="Français"
+          />
+        </video>
+      )}
       <div className={style.containerText}>
         {card.concept && (
           <p>
